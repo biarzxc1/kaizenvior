9a78cb2004f8175a019552d2610799ebb4284ccf
biarzxc1 <biarzxc1@gmail.com>
2025-11-23 20:30:59 +0800

    Create ez.py

---
+++ ez.py
@@ -0,0 +1,73 @@
+# Decoded from marshal encryption
+# Original Author: ANONYMOUS U7P4L
+
+import random
+import os
+import time
+
+os.system('xdg-open https://github.com/ANONYMOUS-U7P4L ')
+os.system('clear')
+
+logo3 = """
+\033[1;99m╔═══════════════════════════════════════════════════╗
+\033[1;92m█   \033[1;91m##     ##\033[1;93m ######## \033[1;94m########  \033[1;95m##        \033[1;32m##       \033[1;92m█
+\033[1;93m█   \033[1;91m##     ##\033[1;93m ##    ## \033[1;94m##     ## \033[1;95m##    ##  \033[1;32m##       \033[1;93m█
+\033[1;94m█   \033[1;91m##     ##\033[1;93m     ##   \033[1;94m##     ## \033[1;95m##    ##  \033[1;32m##       \033[1;94m█
+\033[1;95m█   \033[1;91m##     ##\033[1;93m    ##    \033[1;94m########  \033[1;95m##    ##  \033[1;32m##       \033[1;95m█
+\033[1;96m█   \033[1;91m##     ##\033[1;93m   ##     \033[1;94m##        \033[1;95m######### \033[1;32m##       \033[1;96m█
+\033[1;97m█   \033[1;91m##     ##\033[1;93m   ##     \033[1;94m##        \033[1;95m      ##  \033[1;32m##       \033[1;97m█
+\033[1;91m█    \033[1;91m####### \033[1;93m   ##     \033[1;94m##        \033[1;95m      ##  \033[1;32m######## \033[1;91m█
+\033[1;99m╚═══════════════════════════════════════════════════╝
+[+]==============================================
+[+] CREATED BY   :  ANONYMOUS U7P4L
+[+] COUNTRY      :  BANGLADESH 
+[+] ON GITHUB    :  ANONYMOUS-U7P4L
+[+] TOOL STATUS  :  FIND GIRL FRIEND
+[+] TOOL VIRSION :  7.7
+[+]===============================================
+
+
+  [01] Start GF hack 😚
+  [02] Join My Github Account
+  [03] exit """
+
+print(logo3)
+
+gf = [
+    '\033[1;32m [\033[1;37m+\033[1;32m]\033[1;37m  Your gf ID 🙀 : \033[1;32mhttps://www.facebook.com/profile.php?id=100083879266761&mibextid=ZbWKwL',
+    '\033[1;32m [\033[1;37m+\033[1;32m]\033[1;37m  Your gf 🙀 ID : \033[1;32mhttps://www.facebook.com/profile.php?id=100083645138197&mibextid=ZbWKwL',
+    ' \033[1;32m[\033[1;37m+\033[1;32m]\033[1;37m  Your gf ID 🙀 : \033[1;32mhttps://www.facebook.com/profile.php?id=100083611791512&mibextid=ZbWKwL',
+    ' \033[1;32m[\033[1;37m+\033[1;32m]\033[1;37m  TUI SINGLE MORBI 😒',
+    '\033[1;32m [\033[1;37m+\033[1;32m] \033[1;37m Your gf ID 🔥 : \033[1;32mhttps://www.facebook.com/profile.php?id=100015728161221&mibextid=ZbWKwL',
+    '\033[1;32m [\033[1;37m+\033[1;32m]\033[1;37m  TOMAR VAGGE GF NAI TUMI HAT MARE GHUMAI PORPORO ☹️',
+    ' \033[1;32m [\033[1;37m+\033[1;32m]\033[1;37m TOR GF ONNO BEDA NIYA PALAY GESE 🙂',
+    '  \033[1;32m[\033[1;37m+\033[1;32m] \033[1;37mYour gf ID 🙀 : \033[1;32mhttps://www.facebook.com/profile.php?id=100073296248219&mibextid=ZbWKwL',
+    '  \033[1;32m[\033[1;37m+\033[1;32m] \033[1;37mYour gf ID 🥵 : \033[1;32mhttps://www.facebook.com/profile.php?id=100083787315350&mibextid=ZbWKwL',
+    '  \033[1;32m[\033[1;37m+\033[1;32m] \033[1;37mYour gf ID 🔥 : \033[1;32mhttps://www.facebook.com/profile.php?id=100084336214008&mibextid=ZbWKwL',
+    '  \033[1;32m[\033[1;37m+\033[1;32m] \033[1;37mYour gf ID 🔥 : \033[1;32mhttps://www.facebook.com/xannat.mallik998?mibextid=ZbWKwL',
+    ' \033[1;32m [\033[1;37m+\033[1;32m]\033[1;37m Your gf ID 😚 : \033[1;32mhttps://www.facebook.com/profile.php?id=100087759677318&mibextid=ZbWKwL',
+    ' \033[1;32m [\033[1;3um+\033[1;32m] \033[1;37mHAT MARE GHUMAI POR TOR VAGGE GF NAI 🥹'
+]
+
+iccha = input('\n\n  [+] YOUR CHOICE : ')
+iccha = iccha.replace(' ', '')
+
+if iccha == '3' or iccha == '03':
+    os.syatem('exit')
+
+if iccha == '2' or iccha == '02':
+    os.system('xdg-open https://github.com/ANONYMOUS-U7P4L ')
+
+if iccha == '1' or iccha == '02':
+    vaggo = random.choice(gf)
+    os.system('clear')
+    print("""
+
+╔════════════════════[ NOTICE  ]════════════════════╗
+║ TOMAR GF SMS RIPLEY NA DILE SETA TOMAR BERTHOTA   ║
+║ TUMI TAKE POTAITE PARO NA ADMIN ER JONNO DAI NA   ║
+╚═══════════════════════════════════════════════════╝
+  
+  """)
+    time.sleep(1)
+    print(vaggo)
